4b90d8737bfdd4649858114203964ed1b19b60b1
Namrata Tiwari <sweetytiwari80090@gmail.com>
2025-08-10 19:22:43 +0530

    adding new solution

---
+++ GFG 160Days_DSA/Strings/H-index.cpp
@@ -0,0 +1,42 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int hIndex(vector<int> &citations)
+{
+
+  int n = citations.size();
+  vector<int> freq(n + 1);
+
+  // count the frequency of citations
+  for (int i = 0; i < n; i++)
+  {
+    if (citations[i] >= n)
+      freq[n] += 1;
+    else
+      freq[citations[i]] += 1;
+  }
+
+  int idx = n;
+
+  // variable to keep track of the count of papers
+  // having at least idx citations
+  int s = freq[n];
+  while (s < idx)
+  {
+    idx--;
+    s += freq[idx];
+  }
+
+  // return the largest index for which the count of
+  // papers with at least idx citations becomes >= idx
+  return idx;
+}
+
+int main()
+{
+  vector<int> citations = {6, 0, 3, 5, 3};
+  cout << hIndex(citations) << "\n";
+  return 0;
+}
